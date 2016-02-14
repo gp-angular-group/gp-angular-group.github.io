64f91dba83c32f695401604e62ec931dbd23f607
Ech0oo <Ech0oo@tut.by>
2016-02-14 17:09:53 +0300

    Set router with 2 pages: login(default) and profile

--- src/app/index.route.js
+++ src/app/index.route.js
@@ -1,21 +1,34 @@
 (function() {
-  'use strict';
+	'use strict';
 
-  angular
-    .module('gpSocial')
-    .config(routerConfig);
+	angular
+		.module('gpSocial')
+		.config(routerConfig);
 
-  /** @ngInject */
-  function routerConfig($stateProvider, $urlRouterProvider) {
-    $stateProvider
-      .state('home', {
-        url: '/',
-        templateUrl: 'app/main/main.html',
-        controller: 'MainController',
-        controllerAs: 'main'
-      });
+	/** @ngInject */
+	function routerConfig($stateProvider, $urlRouterProvider) {
+		$stateProvider
+//			.state('home', {
+//				url: '/',
+//				templateUrl: 'app/main/main.html',
+//				controller: 'MainController',
+//				controllerAs: 'main'
+//			})
+			.state('login', {
+				url: '/',
+				templateUrl: 'app/login/login.html',
+				controller: 'LoginController',
+				controllerAs: 'login'
+			})
 
-    $urlRouterProvider.otherwise('/');
-  }
+			.state('profile', {
+				url: '/profile',
+				templateUrl: 'app/profile/profile.html',
+				controller: 'ProfileController',
+				controllerAs: 'profile'
+			});
+
+		$urlRouterProvider.otherwise('/');
+	}
 
 })();
