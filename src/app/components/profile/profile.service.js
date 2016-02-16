341abe9158fd78f7cd5a4331867be576d58056c3
Ech0oo <Ech0oo@tut.by>
2016-02-16 21:27:06 +0300

    mend

--- src/app/components/profile/profile.service.js
+++ src/app/components/profile/profile.service.js
@@ -19,7 +19,18 @@
     };
 
     this.saveProfile = function(user) {
-      return $http.post('https://fathomless-everglades-3680.herokuapp.com/api/user/1', user);
+      var userSend = {
+        avatar: user.avatar,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        age: user.age
+      }
+      if (user.gender == 'Female'){
+        userSend.gender =  'F';
+      } else{
+        userSend.gender =  'M';
+      }
+      return $http.post('https://fathomless-everglades-3680.herokuapp.com/api/user/1', userSend);
     }
   }
 
