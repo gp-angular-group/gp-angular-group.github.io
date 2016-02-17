3a83e3c48ddc615e5918f83a27a6be920fa38c2b
serpento <simsonne@tut.by>
2016-02-18 00:17:10 +0300

    Use keys to show genders

--- src/app/components/profile/profile.service.js
+++ src/app/components/profile/profile.service.js
@@ -4,44 +4,18 @@
   angular
     .module('gpSocial')
     .service('profileService', profileService)
-/*    .service('profileServiceMock', profileServiceMock)
-    .config(function($provide){
-      $provide.decorator('profileService', function($delegate, profileServiceMock) {
-        $delegate = profileServiceMock;
-        return $delegate;
-      });
-    });*/
 
   /** @ngInject */
   function profileService($http) {
     this.getProfile = function() {
-      return $http.get('https://fathomless-everglades-3680.herokuapp.com/api/user/1');
+      return $http.get('https://fathomless-everglades-3680.herokuapp.com/api/user/1')
+        .then(function(response) {
+          return response.data;
+        });
     };
 
     this.saveProfile = function(user) {
-      var userSend = {
-        avatar: user.avatar,
-        firstName: user.firstName,
-        lastName: user.lastName,
-        age: user.age
-      }
-      if (user.gender == 'Female'){
-        userSend.gender =  'F';
-      } else{
-        userSend.gender =  'M';
-      }
-      return $http.post('https://fathomless-everglades-3680.herokuapp.com/api/user/1', userSend);
+      return $http.post('https://fathomless-everglades-3680.herokuapp.com/api/user/1', user);
     }
   }
-
-  /** @ngInject */
-  /*function profileServiceMock($http) {
-    this.getProfile = function() {
-      return $http.get('app/components/profile/mock/getProfile.mock.json');
-    };
-
-    this.saveProfile = function() {
-      return $http.get('app/components/profile/mock/saveProfile.mock.json');
-    };
-  }*/
 })();
--- src/app/profile/profile.controller.js
+++ src/app/profile/profile.controller.js
@@ -9,23 +9,13 @@
   function ProfileController(profileService, $log){
     var vm = this;
 
-    vm.genders = [ 'Male', 'Female' ];
+    vm.genders = {
+      M: 'Male',
+      F: 'Female'
+    };
 
-    profileService.getProfile().then(function(response) {
-      vm.user = {
-        firstName: response.data.firstName,
-        lastName: response.data.lastName,
-        age: response.data.age,
-        //gender: response.data.gender,
-        avatar: response.data.avatar
-      }
-
-      if (response.data.gender == "M"){
-        vm.user.gender = 'Male';
-      }else{
-        vm.user.gender = 'Female';
-      }
-
+    profileService.getProfile().then(function(user) {
+      vm.user = user;
     });
 
     vm.save = function() {
