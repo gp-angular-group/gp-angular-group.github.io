7ad53ca99a0eed7746a52fff9e8cf60de6a5974e
Ech0oo <Ech0oo@tut.by>
2016-02-17 23:08:19 +0300

    insert an empty string after object user

--- src/app/profile/profile.controller.js
+++ src/app/profile/profile.controller.js
@@ -1,39 +1,40 @@
 (function() {
-  'use strict';
+	'use strict';
 
-  angular
-    .module('gpSocial')
-    .controller('ProfileController', ProfileController);
-  
-  /** @ngInject */
-  function ProfileController(profileService, $log){
-    var vm = this;
+	angular
+		.module('gpSocial')
+		.controller('ProfileController', ProfileController);
 
-    vm.genders = [ 'Male', 'Female' ];
+	/** @ngInject */
+	function ProfileController(profileService, $log){
+		var vm = this;
 
-    profileService.getProfile().then(function(response) {
-      vm.user = {
-        firstName: response.data.firstName,
-        lastName: response.data.lastName,
-        age: response.data.age,
-        //gender: response.data.gender,
-        avatar: response.data.avatar
-      }
-      if (response.data.gender == "M"){
-        vm.user.gender = 'Male';
-      }else{
-        vm.user.gender = 'Female';
-      }
-   
-    });
+		vm.genders = [ 'Male', 'Female' ];
 
-    vm.saveClick = function() {
-      $log.info("save");
-      profileService.saveProfile(vm.user).then(function(response){
-        if(response.data.success) {
-          vm.message = "Saved!";
-        }
-      });
-    }
-  }
+		profileService.getProfile().then(function(response) {
+			vm.user = {
+				firstName: response.data.firstName,
+				lastName: response.data.lastName,
+				age: response.data.age,
+				//gender: response.data.gender,
+				avatar: response.data.avatar
+			}
+
+			if (response.data.gender == "M"){
+				vm.user.gender = 'Male';
+			}else{
+				vm.user.gender = 'Female';
+			}
+
+		});
+
+		vm.saveClick = function() {
+			$log.info("save");
+			profileService.saveProfile(vm.user).then(function(response){
+				if(response.data.success) {
+					vm.message = "Saved!";
+				}
+			});
+		}
+	}
 })();
