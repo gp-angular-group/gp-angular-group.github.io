9a55dc57c3000ef47dfb621ae9fb27396bec64d5
Vegasok <lamps23@mail.ru>
2016-02-18 14:04:47 +0300

    removed the 'burger' from start page

--- src/app/components/navbar/navbar.controller.js
+++ src/app/components/navbar/navbar.controller.js
@@ -26,13 +26,5 @@
         }
       }
     })
-    
-    .controller('LeftCtrl', function ($scope, $timeout, $mdSidenav, $log) {
-      $scope.close = function () {
-        $mdSidenav('left').close()
-          .then(function () {
-            $log.debug("close LEFT is done");
-          });
-      };
-    })
+        
 })();
