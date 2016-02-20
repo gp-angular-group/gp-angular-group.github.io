8644e2e3b9bdacecf60db0342f518d089527d34f
AlexeiRAP@gmail.com <AlexeiRAP@gmail.com>
2016-02-20 14:41:40 +0300

    fix history page: delete a price button, creates some history events

--- src/app/history/history.controller.js
+++ src/app/history/history.controller.js
@@ -9,12 +9,20 @@
   function HistoryController($log){
     var vm = this;
 
-    vm.myhistory = {
+    vm.myhistory = [
+      {
     	image: 'http://38.media.tumblr.com/avatar_8cc900efa7d0_128.png',
-    	title:'Мой заголовок',
-    	description: 'Моё описание исторического события',
+    	title:'Мой заголовок 1',
+    	description: 'Моё описание 1',
     	price: 150
-    }
+      },
+      {
+      image: 'http://38.media.tumblr.com/avatar_8cc900efa7d0_128.png',
+      title:'Мой заголовок 2',
+      description: 'Моё описание 2',
+      price: 300  
+      }
+    ]
 
     vm.sort = function(){
     	$log.info('Сортировка');
