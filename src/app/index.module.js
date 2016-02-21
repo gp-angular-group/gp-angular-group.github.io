f4ebdfa221280d8152bf1e1cd2e9cad358f5006d
Ech0oo <Ech0oo@tut.by>
2016-02-21 23:33:15 +0300

    fake backend sever for messages

--- src/app/index.module.js
+++ src/app/index.module.js
@@ -1,18 +1,18 @@
 (function() {
-	'use strict';
+  'use strict';
 
-	angular
-		.module('gpSocial', [
-			'ngAnimate',
-			'ngCookies',
-			'ngTouch',
-			'ngSanitize',
-			'ngMessages',
-			'ngAria',
-			'ui.router',
-			'ngMaterial',
-			'toastr',
-			'angAccordion'
-		]);
-
+  angular
+    .module('gpSocial', [
+      'ngMockE2E',
+      'ngAnimate',
+      'ngCookies',
+      'ngTouch',
+      'ngSanitize',
+      'ngMessages',
+      'ngAria',
+      'ui.router',
+      'ngMaterial',
+      'toastr',
+      'angAccordion'
+    ]);
 })();
